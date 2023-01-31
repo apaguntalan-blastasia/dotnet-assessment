fb1e18bd9e24ac01cbd0c2225ccb1fe16eed9257
Adrian Rei N. Paguntalan <adrianpaguntalan@gmail.com>
2023-01-31 21:02:23 +0800

    test commit

--- DotnetAssessment/DotnetAssessment.WEB/WeatherForecast.cs
+++ DotnetAssessment/DotnetAssessment.WEB/WeatherForecast.cs
@@ -2,6 +2,7 @@ namespace DotnetAssessment.WEB
 {
     public class WeatherForecast
     {
+        //Test
         public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
